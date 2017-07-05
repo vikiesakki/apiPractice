766780e25d8e04b693bd52f4795fb6887735c76f
vikiesakki <vemcse@gmail.com>
2017-07-05 12:27:32 +0530

    Swagger Bug

--- config/initializers/swagger_docs.rb
+++ config/initializers/swagger_docs.rb
@@ -1,11 +1,16 @@
- 
+ class Swagger::Docs::Config
+  def self.transform_path(path, api_version)
+    "http://localhost:3000/#{path}"
+  end
+end
 Swagger::Docs::Config.register_apis({
   "1.0" => {
     # the extension used for the API
     # the output location where your .json files are written to
-    :api_file_path => "public/",
+    :api_extension_type => :json,
+    :api_file_path => "public",
     :base_path => "http://localhost:3000/api",
-    :clean_directory => true,
+    :clean_directory => false,
     # if you want to delete all .json files at each generation
     # Ability to setup base controller for each api version. Api::V1::SomeController for example.
     # add custom attributes to api-docs
